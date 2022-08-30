0061c647f2ee51f9fb35e26dca048db240e669cc
Alex Pages <alexpagesandreu@gmail.com>
2022-08-30 08:11:52 +0200

    Modification:
    1- Created function newCasesCountry to delegate http request
    2- Modified main function so the user can input a country and the http request will be modified to look for information of the country

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,30 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.fasterxml.jackson.databind.ObjectMapper; // version 2.11.1
-
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
 
+        //String Builder for user input
+        StringBuilder uriHttp = new StringBuilder();
+        Scanner console = new Scanner(System.in);
+        System.out.println("Please enter a valid country name: ");
+        String country = console.nextLine();
+        uriHttp.append("https://covid-193.p.rapidapi.com/statistics?country=");
+        uriHttp.append(country.toLowerCase());
+
+        //Calling function
+        System.out.println(newCasesCountry(uriHttp.toString()));
+    }
+
+    public static String newCasesCountry(String uriHttp) throws IOException, InterruptedException {
+
         //HTTP Request to covid API
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://covid-193.p.rapidapi.com/statistics?country=spain"))
+                .uri(URI.create(uriHttp))
                 .header("X-RapidAPI-Key", "2485024da0mshac7e7509945a67ep1735eajsna56b007928ad")
                 .header("X-RapidAPI-Host", "covid-193.p.rapidapi.com")
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -24,17 +37,15 @@ public class Main {
         System.out.println(response.body());
 
         //Parse JSON into object
-        try{
+        Root root = null;
+        try {
             ObjectMapper om = new ObjectMapper();
-            Root root = om.readValue(response.body(), Root.class);
-            System.out.println(root.response.get(0).cases.mynew);
-        } catch (Exception e){
+            root = om.readValue(response.body(), Root.class);
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
+        return root.response.get(0).cases.mynew;
     }
-
-
-
 
 }
